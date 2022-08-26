d21f8e9c527c6f4bb9f1513877a68dda51e4345f
M-Faheem-Khan <faheem5948@gmail.com>
2022-08-25 22:45:58 -0400

    Add: Header & Function Comments

--- DNSRecon/cmd/main.go
+++ DNSRecon/cmd/main.go
@@ -1,3 +1,7 @@
+// Author: M-Faheem-Khan
+// File: DNSRecon/cmd/main.go
+// Description: Lists A, AAAA, CNAME, NameServer, TXT dns records
+
 package main
 
 import (
@@ -7,13 +11,14 @@ import (
 )
 
 func main() {
+	// Verifying domain name is given as an arg
 	if len(os.Args) != 2 {
 		fmt.Println("Lists A, AAAA, CNAME, NameServer, TXT dns records")
 		fmt.Printf("Usage: %s domain\n", os.Args[0])
-		fmt.Println(os.Args)
 		os.Exit(0)
 	}
 
+	// Getting the domain name
 	domain := os.Args[1]
 	coloredOutput := fmt.Sprintf("\x1b[%dm%s%s\x1b[0m", 33, "Domain: ", domain)
 	fmt.Println(coloredOutput)
@@ -26,6 +31,7 @@ func main() {
 	getTxtRecords(domain)
 }
 
+// List A/AAAA records of a given domain
 func getIP(domain string) {
 	ipRecords, _ := net.LookupIP(domain)
 	coloredOutput := fmt.Sprintf("\x1b[%dm%s\x1b[0m", 33, "=== A/AAAA Records ===")
@@ -35,6 +41,7 @@ func getIP(domain string) {
 	}
 }
 
+// List MX records of a given domain
 func getMXRecords(domain string) {
 	mxRecords, _ := net.LookupMX(domain)
 	coloredOutput := fmt.Sprintf("\x1b[%dm%s\x1b[0m", 33, "=== MX Records ===")
@@ -44,6 +51,7 @@ func getMXRecords(domain string) {
 	}
 }
 
+// List CNAME record of a given domain
 func getCNAME(domain string) {
 	cname, _ := net.LookupCNAME(domain)
 	coloredOutput := fmt.Sprintf("\x1b[%dm%s\x1b[0m", 33, "=== CNAME Record ===")
@@ -51,6 +59,7 @@ func getCNAME(domain string) {
 	fmt.Println(cname)
 }
 
+// List Name Server records of a given domain
 func getNameServers(domain string) {
 	nameservers, _ := net.LookupNS(domain)
 	coloredOutput := fmt.Sprintf("\x1b[%dm%s\x1b[0m", 33, "=== Name Server Records ===")
@@ -60,6 +69,7 @@ func getNameServers(domain string) {
 	}
 }
 
+// List TXT records of a given domain
 func getTxtRecords(domain string) {
 	txtRecords, _ := net.LookupTXT(domain)
 	coloredOutput := fmt.Sprintf("\x1b[%dm%s\x1b[0m", 33, "=== TXT Records ===")
